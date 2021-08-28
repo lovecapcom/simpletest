87795b7e4dc038a1b9f14c6de1eb7f6df13cb7c2
lovecapcom <lovecapcom@hotmail.com>
2021-08-28 14:03:48 -0700

    so1only

--- test/test.cc
+++ test/test.cc
@@ -8,6 +8,36 @@
 #include "../so1/myhashmap.hpp"
 
 
+class Basic {
+public:
+    Basic(const std::string str, int i = 0) : name(str), count(i) {
+        std::cout << this->name << " constructure invoked!" << std::endl;
+     }
+    Basic(const Basic& b) { this->name = b.name; }
+    Basic& add() {
+        this->count++;
+        return *this;
+    }
+    int getCount() {
+        return this->count;
+    }
+    ~Basic() { 
+        std::cout << this->name << " destructure invoded!" << std::endl;
+    }
+private:
+    std::string name;
+    int count;
+};
+
+const double PI = 3.14;
+
+double area(double radius) {
+    return PI * radius * radius;
+}
+
+double circumference(double radius) {
+    return PI * radius * 2;
+}
 
 int main() {
     
@@ -40,26 +70,31 @@ int main() {
 //    std::cout << "===========寻找字符串============" << std::endl << s2.find("456", 0) << std::endl;
 
 
-	MyHashMap<std::string, std::string, HashFunc, EqualKey> hashmap(100);
-
-    hashmap.insert("hello", "world");
-    hashmap.insert("why", "dream");
-    hashmap.insert("c++", "good");
-    hashmap.insert("welcome", "haha");
+//	MyHashMap<std::string, std::string, HashFunc, EqualKey> hashmap(100);
+//
+//    hashmap.insert("hello", "world");
+//    hashmap.insert("why", "dream");
+//    hashmap.insert("c++", "good");
+//    hashmap.insert("welcome", "haha");
+//
+//    
+//    std::cout << "after insert:" << std::endl;
+//    std::cout << hashmap.find("welcome").c_str() << std::endl;
+//    std::cout << hashmap.find("c++").c_str() << std::endl;
+//    std::cout << hashmap["why"].c_str() << std::endl;
+//    std::cout << hashmap["hello"].c_str() << std::endl;
+//
+//    if (hashmap.del("hello"))
+//        std::cout << "remove is ok" << std::endl;    //remove is ok
+//    std::cout << hashmap.find("hello").c_str() << std::endl; //not exist print NULL
+//
+//    hashmap["why"] = "love";
+//    std::cout << hashmap["why"].c_str() << std::endl;
 
     
-    std::cout << "after insert:" << std::endl;
-    std::cout << hashmap.find("welcome").c_str() << std::endl;
-    std::cout << hashmap.find("c++").c_str() << std::endl;
-    std::cout << hashmap["why"].c_str() << std::endl;
-    std::cout << hashmap["hello"].c_str() << std::endl;
-
-    if (hashmap.del("hello"))
-        std::cout << "remove is ok" << std::endl;    //remove is ok
-    std::cout << hashmap.find("hello").c_str() << std::endl; //not exist print NULL
-
-    hashmap["why"] = "love";
-    std::cout << hashmap["why"].c_str() << std::endl;
+    int a = 0;
+    int b = a + 1 + 1;
+    std::cout << b << std::endl;
 
     return 0;
 }
